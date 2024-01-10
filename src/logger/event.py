f32e32657d8fc33dc64fd929d03c460feec3215d
Merok23 <alejocalizblanco@gmail.com>
2024-01-10 14:17:33 -0300

    I refactorized the logger, now you just execute the main file with the coordinates of the map you are trying to store, and then when you are finished, you press the escape key.
    I want to do the same with the "player" I want a main file that you can interrupt and call with
    some coordinates or boundaries and it will just cycle from where it's called, whatever is more comfy.

---
+++ src/logger/event.py
@@ -0,0 +1,54 @@
+# event.py
+"""
+This class is only for storing events.
+"""
+
+
+class Event:
+    """
+    The idea of the event is:
+    - event_type: keyboard or mouse
+    - event_subtype: press, release, click, scroll
+    - event_data: key or mouse data
+        For key press/release: key
+        For mouse click: (x, y, button, pressed)
+    - event_time: time since the last event
+    """
+
+    def __init__(self, event_type, event_subtype, event_data, event_time):
+        self.event_type = event_type
+        self.event_subtype = event_subtype
+        self.event_data = event_data
+        self.event_time = event_time
+
+    def get_event_type(self):
+        """
+        Returns the event type.
+
+        :return: event type (str, "keyboard" or "mouse")
+        """
+        return self.event_type
+
+    def get_event_subtype(self):
+        """
+        Returns the event subtype.
+
+        :return: event subtype (str, "press", "release", "click", "scroll")
+        """
+        return self.event_subtype
+
+    def get_event_data(self):
+        """
+        Returns the event data.
+
+        :return: event data (varies based on event type and subtype)
+        """
+        return self.event_data
+
+    def get_event_time(self):
+        """
+        Returns the event time.
+
+        :return: event time (float)
+        """
+        return self.event_time
---
+++ src/logger/map_logger.py
@@ -0,0 +1,41 @@
+# map_logger.py
+"""
+Map logger class.
+It's used to create a single csv of a single map using the logger.
+"""
+from event_logger import Logger
+
+
+class MapLogger:
+    """
+    This class is used to create a single csv of a single map using the logger.
+    """
+
+    def __init__(self, map_coordinates):
+        """
+        Initialize the map logger.
+        :param map_coordinates: tuple of (x, y) coordinates of the map
+        """
+        self._logger = Logger()
+        self._map_coordinates = map_coordinates
+
+    def start_logging(self):
+        """
+        Start logging the map.
+        """
+        self._logger.start_logging()
+
+    def stop_logging(self):
+        """
+        Stop logging the map.
+        """
+        self._logger.stop_logging()
+
+    def dump_to_csv(self, file_path):
+        """
+        Dump the recorded events to a csv file.
+
+        :param file_path: path to the directory you want to save the csv file in
+        """
+        file_name = f"{self._map_coordinates[0]}_{self._map_coordinates[1]}.csv"
+        self._logger.dump_to_csv(f"{file_path}/{file_name}")
